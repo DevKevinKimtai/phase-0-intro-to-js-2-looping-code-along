9ae80206b07c05d8e0e21926c88cfc1e23730559
Kevin <kimutaimoringa0001@gmail.com>
2025-03-26 00:33:11 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// write cards function Task 1
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+  // Task 2: countDown() function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
